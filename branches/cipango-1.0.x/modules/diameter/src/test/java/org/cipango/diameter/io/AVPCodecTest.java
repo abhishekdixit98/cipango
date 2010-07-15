755edb327ceb39e3f36a55dd27ba31ba5c8a1272
nicolas@cipango.org <nicolas@cipango.org@657f710f-be33-0410-a596-71a6d81f68ef>
2010-07-15 08:49:51 +0000

    Fix CIPANGO-85: Padding bytes in diameter are not set to zero

--- branches/cipango-1.0.x/modules/diameter/src/test/java/org/cipango/diameter/io/AVPCodecTest.java
+++ branches/cipango-1.0.x/modules/diameter/src/test/java/org/cipango/diameter/io/AVPCodecTest.java
@@ -3,9 +3,11 @@ package org.cipango.diameter.io;
 import java.io.IOException;
 
 import org.cipango.diameter.AVP;
+import org.cipango.diameter.base.Base;
 import org.cipango.diameter.ims.Cx;
 import org.mortbay.io.Buffer;
 import org.mortbay.io.ByteArrayBuffer;
+import org.mortbay.io.View;
 
 import junit.framework.TestCase;
 
@@ -22,6 +24,29 @@ public class AVPCodecTest extends TestCase
 		
 		assertEquals(avp.getType().getCode(), decoded.getType().getCode());
 		assertEquals(avp.getType().getVendorId(), decoded.getType().getVendorId());
+		
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void testPadding() throws IOException
+	{
+		byte[] b = { 13 };
+		AVP<byte[]> avp = new AVP<byte[]>(Cx.INTEGRITY_KEY, b);
+		Buffer buffer = new ByteArrayBuffer(64);
+		for (int i = 0; i < 64; i++)
+			buffer.put((byte) 44);
+		buffer.setPutIndex(0);
+		Codecs.__avp.encode(buffer, avp);
+		View view = new View(buffer);
+		view.setGetIndex(view.putIndex() - 3);
+		for (int i = 0; i < 3; i++)
+			assertEquals(0, view.get());
+		
+		AVP<byte[]> decoded = (AVP<byte[]>) Codecs.__avp.decode(buffer);
+		
+		assertEquals(avp.getType().getCode(), decoded.getType().getCode());
+		assertEquals(avp.getType().getVendorId(), decoded.getType().getVendorId());
+		assertEquals(avp.getValue()[0], decoded.getValue()[0]);
 		
 	}
 }
