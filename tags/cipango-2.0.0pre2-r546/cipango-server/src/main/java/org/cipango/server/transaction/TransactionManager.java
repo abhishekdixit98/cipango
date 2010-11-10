ae694920fd3ee36911857a497c2664cde92ddf80
nicolas@cipango.org <nicolas@cipango.org@657f710f-be33-0410-a596-71a6d81f68ef>
2010-11-10 13:57:06 +0000

    cipango-2.0.0pre2-r546

---
+++ tags/cipango-2.0.0pre2-r546/cipango-server/src/main/java/org/cipango/server/transaction/TransactionManager.java
@@ -0,0 +1,261 @@
+// ========================================================================
+// Copyright 2008-2009 NEXCOM Systems
+// ------------------------------------------------------------------------
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at 
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ========================================================================
+
+package org.cipango.server.transaction;
+
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicLong;
+
+import javax.servlet.ServletException;
+import javax.servlet.sip.SipServletMessage;
+import javax.servlet.sip.SipServletResponse;
+
+import org.cipango.server.SipHandler;
+import org.cipango.server.SipMessage;
+import org.cipango.server.SipProxy;
+import org.cipango.server.SipRequest;
+import org.cipango.server.SipResponse;
+import org.cipango.server.session.Session;
+import org.cipango.sip.SipGrammar;
+
+import org.eclipse.jetty.server.handler.HandlerWrapper;
+import org.eclipse.jetty.util.log.Log;
+import org.eclipse.jetty.util.statistic.CounterStatistic;
+
+public class TransactionManager extends HandlerWrapper implements SipHandler
+{   
+    private final AtomicLong _statsStartedAt = new AtomicLong(-1L);
+    
+    private CounterStatistic _retransStats = new CounterStatistic();
+    private CounterStatistic _notFoundStats = new CounterStatistic();
+	
+	public void handle(SipServletMessage message) throws ServletException, IOException 
+    {
+		if (((SipMessage) message).isRequest())
+			handleRequest((SipRequest) message);
+		else
+			handleResponse((SipResponse) message);
+	}
+	
+	public void handleRequest(SipRequest request) throws ServletException, IOException 
+    {
+		String branch = request.getTopVia().getBranch();
+		
+		if (branch == null || !branch.startsWith(SipGrammar.MAGIC_COOKIE)) 
+        {
+			if (!("0".equals(branch) && request.isAck()))
+			{
+				Log.debug("Not 3261 branch: {}. Dropping request", branch);
+				return;
+			}
+		}
+		if (request.isCancel()) 
+			branch = "cancel-" + branch;
+		
+		ServerTransaction transaction = request.getCallSession().getServerTransaction(branch);
+		
+		if (transaction != null) 
+        {
+            if (Log.isDebugEnabled()) 
+                Log.debug("request {} in transaction {}", request.getRequestLine(), transaction);
+			
+            request.setTransaction(transaction);
+            if (request.isAck())
+            {
+            	transaction.handleAck(request);
+            }
+            else
+            {
+            	retransReceived();
+            	transaction.handleRetransmission(request);
+            }
+		} 
+		else
+		{
+			transaction = new ServerTransaction(request);
+
+			if (!request.isAck()) 
+				request.getCallSession().addServerTransaction(transaction);
+		 
+	        if (Log.isDebugEnabled())
+	            Log.debug("new transaction {} for request {}", transaction, request.getRequestLine());
+	
+	        // TODO move to Session
+			if (request.isCancel())
+	        {
+	            String txBranch = request.getTopVia().getBranch();
+	            ServerTransaction stx = request.getCallSession().getServerTransaction(txBranch);
+	            if (stx == null)
+	            {
+	                if (Log.isDebugEnabled())
+	                    Log.debug("No transaction for cancelled branch {}", txBranch, null);
+	                SipResponse unknown = (SipResponse) request.createResponse(SipServletResponse.SC_CALL_LEG_DONE);
+	                transaction.send(unknown);
+	            }
+	            else 
+	            {
+	                SipResponse ok = (SipResponse) request.createResponse(SipServletResponse.SC_OK);
+	                transaction.send(ok);
+	                
+	                stx.cancel(request);
+	            }
+	        }
+	        else 
+	        	((SipHandler) getHandler()).handle(request);
+		}
+	}
+	
+	public void handleResponse(SipResponse response) throws ServletException, IOException
+    {
+		String branch = response.getTopVia().getBranch();
+        
+		if (response.isCancel()) 
+			branch = "cancel-" + branch;
+		
+		ClientTransaction ctx = response.getCallSession().getClientTransaction(branch);
+
+		if (ctx == null)
+		{
+			if (Log.isDebugEnabled())
+				Log.debug("did not find client transaction for response {}", response);
+			
+			transactionNotFound();
+			return;
+		}
+		
+		if (Log.isDebugEnabled())
+            Log.debug("response {} for transaction {}", response, ctx);
+		
+		response.setTransaction(ctx);
+		ctx.handleResponse(response);
+    }
+	
+	public ClientTransaction sendRequest(SipRequest request, ClientTransactionListener listener) 
+    {
+		ClientTransaction ctx = new ClientTransaction(request, listener);
+		
+		if (!request.isAck())
+			request.getCallSession().addClientTransaction(ctx);
+		
+		try 
+        {
+			ctx.start();
+		} 
+        catch (IOException e)
+        {
+			Log.warn(e);
+		}
+		return ctx;
+	}
+	
+	public int getT1() { return Transaction.__T1; }
+	public int getT2() { return Transaction.__T2; }
+	public int getT4() { return Transaction.__T4; }
+	public int getTD() { return Transaction.__TD; }
+	public int getTimerC() { return SipProxy.__timerC; }
+	
+	public void setT1(int millis)
+	{ 
+		if (millis < 0)
+			throw new IllegalArgumentException("SIP Timers must be positive");
+		Transaction.__T1 = millis;
+	}
+	
+	public void setT2(int millis) 
+	{
+		if (millis < 0)
+			throw new IllegalArgumentException("SIP Timers must be positive");
+		Transaction.__T2 = millis;
+	}
+	
+	public void setT4(int millis) 
+	{
+		if (millis < 0)
+			throw new IllegalArgumentException("SIP Timers must be positive");
+		Transaction.__T4 = millis;
+	}
+	
+	public void setTD(int millis) 
+	{
+		if (millis < 0)
+			throw new IllegalArgumentException("SIP Timers must be positive");
+		Transaction.__TD = millis;
+	}
+	
+	public void setTimerC(int millis) 
+	{
+		if (millis < 0)
+			throw new IllegalArgumentException("SIP Timers must be positive");
+		SipProxy.__timerC = millis;
+	}
+	
+	protected void retransReceived() 
+	{
+		if (_statsStartedAt.get() == -1)
+            return;
+		_retransStats.increment();
+	}
+	
+	protected void transactionNotFound()
+	{
+		if (_statsStartedAt.get() == -1)
+			return;
+		_notFoundStats.increment();
+	}
+	
+	public long getRetransmissions()
+	{
+		return _retransStats.getCurrent();
+	}
+	
+	public long getNotFoundTransactions()
+	{
+		return _notFoundStats.getCurrent();
+	}
+	
+	public void statsReset()
+    {
+        updateNotEqual(_statsStartedAt,-1,System.currentTimeMillis());
+
+        _retransStats.reset();
+        _notFoundStats.reset();
+    }
+	
+	public void setStatsOn(boolean on)
+    {
+        if (on && _statsStartedAt.get() != -1)
+            return;
+
+        Log.debug("Statistics on = " + on + " for " + this);
+
+        statsReset();
+        _statsStartedAt.set(on?System.currentTimeMillis():-1);
+    }
+	
+	public boolean getStatsOn()
+    {
+        return _statsStartedAt.get() != -1;
+    }
+	
+	private void updateNotEqual(AtomicLong valueHolder, long compare, long value)
+    {
+        long oldValue = valueHolder.get();
+        while (compare != oldValue)
+        {
+            if (valueHolder.compareAndSet(oldValue,value))
+                break;
+            oldValue = valueHolder.get();
+        }
+    }
+}
---
+++ tags/cipango-2.0.0pre2-r546/extras/sipatra/src/main/java/org/cipango/sipatra/SipatraServlet.java
@@ -0,0 +1,204 @@
+// ========================================================================
+// Copyright 2003-2010 the original author or authors.
+// ------------------------------------------------------------------------
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at 
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ========================================================================
+package org.cipango.sipatra;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.sip.SipServlet;
+import javax.servlet.sip.SipServletMessage;
+import javax.servlet.sip.SipServletRequest;
+import javax.servlet.sip.SipServletResponse;
+
+import org.apache.commons.pool.impl.GenericObjectPool.Config;
+import org.cipango.sipatra.ruby.JRubyRuntimeFactory;
+import org.cipango.sipatra.ruby.JRubyRuntimePool;
+import org.jruby.embed.ScriptingContainer;
+import org.slf4j.Logger;
+
+/**
+ * Sipatra Main Servlet
+ * 
+ * @author Dominique Broeglin <dominique.broeglin@gmail.com>, Jean-Baptiste Morin
+ */
+public class SipatraServlet extends SipServlet 
+{
+	private Logger _log = org.slf4j.LoggerFactory.getLogger(SipatraServlet.class);
+
+	private static final String SIPATRA_PATH_PROPERTY 				= "org.cipango.sipatra.script.path";
+	private static final String SIPATRA_POOL_MAX_ACTIVE_PROPERTY 	= "org.cipango.sipatra.pool.maxActive";
+	private static final String SIPATRA_POOL_MAX_IDLE_PROPERTY 		= "org.cipango.sipatra.pool.maxIdle";
+	private static final String SIPATRA_POOL_MAX_WAIT_PROPERTY 		= "org.cipango.sipatra.pool.maxWait";
+	private static final String SIPATRA_POOL_MIN_IDLE_PROPERTY 		= "org.cipango.sipatra.pool.minIdle";
+	private static final String SIPATRA_POOL_MIN_EVICTABLE_PROPERTY	= "org.cipango.sipatra.pool.minEvictableIdleTimeMillis";
+	private static final String SIPATRA_POOL_INIT_POOL_SIZE			= "org.cipango.sipatra.pool.init.size";
+
+	private ServletContext _servletContext;
+
+	private JRubyRuntimePool _pool;
+
+	/**
+	 * Initialize the jrubyServlet.
+	 * 
+	 * @throws ServletException
+	 *             if this method encountered difficulties
+	 */
+	@Override
+	public void init(ServletConfig config) throws ServletException
+	{
+		super.init(config);
+
+		_servletContext = config.getServletContext();
+
+		String appPath = getServletContext().getRealPath("/WEB-INF/sipatra");
+		String scriptPath = getStringProperty(SIPATRA_PATH_PROPERTY, null);
+
+		if (scriptPath == null)
+		{
+			scriptPath = appPath + "/application.rb";
+		}
+		else
+		{
+			File file = new File(scriptPath);
+			if(!file.exists())
+				throw new ServletException(file.getAbsolutePath()+" does not exist!");
+
+			if(file.isFile())
+			{
+				if(!file.getName().endsWith(".rb"))
+					_log.warn(file.getAbsolutePath()+" is not a ruby file!");
+
+				if(file.getParentFile() != null)
+					appPath = file.getParentFile().getAbsolutePath();
+				else
+					_log.error(file.getAbsolutePath()+" got no parent directory!");
+			}
+			else if(file.isDirectory())
+			{
+				appPath = new File(scriptPath).getAbsolutePath();
+			}
+		}
+
+		Config conf = new Config();
+
+		conf.maxActive = getIntegerProperty(SIPATRA_POOL_MAX_ACTIVE_PROPERTY, -1);
+		conf.maxIdle = getIntegerProperty(SIPATRA_POOL_MAX_IDLE_PROPERTY, -1);
+		conf.maxWait = getIntegerProperty(SIPATRA_POOL_MAX_WAIT_PROPERTY, -1);
+		conf.minIdle = getIntegerProperty(SIPATRA_POOL_MIN_IDLE_PROPERTY, -1);
+		conf.minEvictableIdleTimeMillis = getIntegerProperty(SIPATRA_POOL_MIN_EVICTABLE_PROPERTY, -1);
+
+		_log.info("Start pool with path: "+appPath+" ...");
+
+		_pool = new JRubyRuntimePool(new JRubyRuntimeFactory(appPath, scriptPath), conf);
+
+		int init_pool_size = getIntegerProperty(SIPATRA_POOL_INIT_POOL_SIZE, 0);
+		for(int i = 0; i< init_pool_size; i++)
+		{
+			try 
+			{
+				_pool.addObject();
+			} 
+			catch (Exception e) 
+			{
+				_log.error("<<ERROR>>", e);
+			}
+		}
+		_log.info("... pool started with "+init_pool_size+" JRuby Runtimes!");
+	}
+
+	private int getIntegerProperty(String name, int defaultValue)
+	{
+		String property = System.getProperty(name);
+		if(property == null || "".equals(property))
+			property = _servletContext.getInitParameter(name);
+		if(property == null || "".equals(property))
+			return defaultValue;
+		else
+		{
+			try
+			{
+				return Integer.valueOf(property);
+			}
+			catch (Exception e) 
+			{
+				_log.warn("Property: "+name+" is not an int. Default value is used.");
+				return defaultValue;
+			}
+		}
+	}
+
+	private String getStringProperty(String name, String defaultValue)
+	{
+		String property = System.getProperty(name);
+		if(property == null || "".equals(property))
+			property = _servletContext.getInitParameter(name);
+		if(property == null || "".equals(property))
+			return defaultValue;
+		else
+			return property;
+	}
+
+	@Override
+	public void doRequest(SipServletRequest request) throws IOException
+	{
+		invokeMethod(request, "do_request");
+	}
+
+	@Override
+	public void doResponse(SipServletResponse response) throws IOException
+	{
+		invokeMethod(response, "do_response");
+	}	
+
+	private void invokeMethod(SipServletMessage message, String methodName) 
+	{
+		ScriptingContainer container = null;
+		try 
+		{
+			container = (ScriptingContainer) _pool.borrowObject();
+			try 
+			{
+				Object app = container.runScriptlet("Sipatra::Application::new");
+
+				container.callMethod(app, "set_bindings", new Object[] { 
+						_servletContext,  
+						_servletContext.getAttribute(SipServlet.SIP_FACTORY), 
+						message.getSession(), 
+						message});
+				container.callMethod(app, methodName);
+			} 
+			catch(Exception e) 
+			{
+				_pool.invalidateObject(container);
+				container = null;
+			} 
+			finally 
+			{
+				if(container != null) 
+				{
+					_pool.returnObject(container);
+				}
+			}
+		} 
+		catch(Exception e) 
+		{
+			_log.error("ERROR >> Failed to borrow a JRuby Runtime ", e);
+			// TODO: failed to borrow a runtime... What should we do?
+			//throw e?
+		}
+	}
+}
