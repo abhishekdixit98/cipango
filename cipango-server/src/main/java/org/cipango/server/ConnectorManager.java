490eecc12b951749d9e317b959675aa9af5a895c
nicolas <nicolas@cipango.org>
2012-04-17 11:43:36 +0000

    CIPANGO-42: the system property sip.mtu is not used

--- cipango-server/src/main/java/org/cipango/server/ConnectorManager.java
+++ cipango-server/src/main/java/org/cipango/server/ConnectorManager.java
@@ -47,9 +47,10 @@ import org.eclipse.jetty.util.log.Log;
 
 public class ConnectorManager extends AbstractLifeCycle implements Buffers, SipHandler
 {
-    private static final int DEFAULT_MTU = 1500;
     private static final int DEFAULT_MESSAGE_SIZE = 16*1024; // FIXME
     private static final int MAX_MESSAGE_SIZE = 64*1024;
+    // By default set MTU to max message size instead of 1500.
+    private static final int DEFAULT_MTU = MAX_MESSAGE_SIZE;
     
     private Server _server;
    
@@ -329,6 +330,39 @@ public class ConnectorManager extends AbstractLifeCycle implements Buffers, SipH
     	
     	try
     	{
+				if (!connection.getConnector().isReliable() 
+        			&& (buffer.putIndex() + 200 > _mtu)
+        			&& message.isRequest()) 
+				{
+    			Log.debug("Message is too large. Switching to TCP");
+					SipConnector connector = findConnector(SipConnectors.TCP_ORDINAL, connection.getRemoteAddress());
+					
+    			if (connector.isReliable())
+    			{
+						try
+						{
+							Via via = message.getTopVia();
+							Via connectorVia = connector.getVia();
+							via.setTransport(connectorVia.getTransport());
+							String host = connectorVia.getHost();
+							via.setHost(host);
+							via.setPort(connectorVia.getPort());
+							SipConnection newConnection = connector.getConnection(connection.getRemoteAddress(), connection.getRemotePort());
+							send(message, newConnection);
+							return;
+						}
+						catch (IOException e) 
+						{
+							Via via = message.getTopVia();
+							// Update via to ensure that right value is used in logs
+							SipConnector connectorOrig = connection.getConnector();
+							via.setTransport(connectorOrig.getTransport());
+							via.setHost(connectorOrig.getSipUri().getHost());
+							via.setPort(connectorOrig.getSipUri().getPort());
+							Log.debug("Failed to switch to TCP, return to original connection");
+						}
+    			}
+    		}
     		connection.write(buffer);
     		
     		if (_accessLog != null)
@@ -747,5 +781,13 @@ public class ConnectorManager extends AbstractLifeCycle implements Buffers, SipH
 		return null;
 	}
 
+		public int getMtu()
+	{
+		return _mtu;
+	}
 
+	public void setMtu(int mtu)
+	{
+		_mtu = mtu;
+	}
 }
