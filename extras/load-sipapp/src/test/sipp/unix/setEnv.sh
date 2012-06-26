71fbcdb3cf7eec45b549cc2c6dd0a0650b562cc1
nicolas <nicolas@cipango.org>
2012-06-26 15:43:33 +0000

    Improve scripts

--- extras/load-sipapp/src/test/sipp/unix/setEnv.sh
+++ extras/load-sipapp/src/test/sipp/unix/setEnv.sh
@@ -4,32 +4,33 @@
 # *************************************************************************
 
 # TO ADAPT
-SIPP_HOME="/usr/local/sipp.svn/"
+SIPP_HOME="/usr/local/sipp.svn"
 
 # The limit for the load message
-NB_LOAD_MESSAGE="5000000"
+NB_LOAD_MESSAGE="5000000000"
+NB_LOAD_MESSAGE="10000000"
 #NB_LOAD_MESSAGE="1"
 
-AS=192.168.2.187:5061
+AS=192.168.2.217
 
-SIPP_IP=192.168.2.187
-SIPP_UAC_IP=${SIPP_IP}
-SIPP_UAS_IP=${SIPP_IP}
+SIPP_UAC_IP=tenerife
+SIPP_UAS_IP=tenerife
 
 UAC_PORT=5062
 
 UAS_PORT=5063
 
-SIPP_OPTIONS=""
+SIPP_OPTIONS="-t u1"
 
 # default -trace_err -trace_screen
 # for debugging  -trace_msg
-TRACE_OPTIONS="-trace_err -trace_screen"
+TRACE_OPTIONS=" -trace_screen"
 
-UAC_SPECIFIC_OPTIONS="-l 200000 -mp 6004"
+UAC_SPECIFIC_OPTIONS="-l 200000"
 
-LOAD_CALL_RATE=1000
-RATE_PERIOD=1000
+
+LOAD_CALL_RATE=100
+RATE_PERIOD=100
 
 echo "The load call rate is set to ${LOAD_CALL_RATE} for a rate period of ${RATE_PERIOD}"
 
